91339c2a9d076bab7a5003b5fe12c472d6cb4f7f
yash joshi <you@example.com>
2021-10-01 12:40:15 +0530

    created lazy loading module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,15 @@ import { RouterModule, Routes } from '@angular/router';
 import { AddDetailsComponent } from './add-details/add-details.component';
 import { AddUserComponent } from './add-user/add-user.component';
 import { HomeComponent } from './home/home.component';
+import { UserModule } from './user/user.module';
 
 const routes: Routes = [
   {path: 'home', component: HomeComponent},
   {path: 'add-user', component: AddUserComponent},
-  {path: 'add-details', component: AddDetailsComponent}
+  {path: 'add-details', component: AddDetailsComponent},
+  { path:'user', loadChildren: ()=>import('./user/user.module')
+  .then(mod => mod.UserModule) 
+  }
 ];
 
 @NgModule({
